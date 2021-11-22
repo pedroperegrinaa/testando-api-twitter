3d60f11b1bba5096d4187e8de0e6f8cafe5d8d62
Rushmerk <57403682+pedroperegrinaa@users.noreply.github.com>
2021-11-22 00:26:44 -0300

    ESSA PORRA NAO TA FUNCIONANDO

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
-const needle = require("needle");
-const express = require("express");
-
-const id = "1034710670829400064";
-const likedTweetsendpointURL = `https://api.twitter.com/2/users/${id}/liked_tweets`;
+const needle = require('needle');
 const token = process.env.BEARER_TOKEN;
 
-function twitterRoute() {
-    const tweets = new express.Router();
+const endpointURL = "https://api.twitter.com/2/tweets?ids=";
 
-    // GET REST endpoint - query params may or may not be populated
-    tweets.get("/", function(req, res) {
-        console.log(new Date(), "In twitter route GET / req.query=", req.query);
+async function getRequest() {
 
-        (async() => {
-            try {
-                // Make request
-                const response = await getLikedTweets();
-
-                //return result
-                console.log("Tweet likes data received");
-                res.json({ response });
-            } catch (e) {
-                console.log(e);
-            }
-        })();
-    });
-
-    async function getLikedTweets() {
-        // The default parameters - only the Tweet ID and text are returned
-        const params = {
-            "tweet.fields": "lang,author_id",
-            "user.fields": "created_at",
-        };
-
-        // this is the HTTP header that adds bearer token authentication
-        const res = await needle("get", likedTweetsendpointURL, params, {
-            headers: {
-                "User-Agent": "LikedTweetsTestCode",
-                authorization: `Bearer ${token}`,
-            },
-        });
-
-        if (res.body) {
-            return res.body;
-        } else {
-            throw new Error("Unsuccessful request");
-        }
+    const params = {
+        "ids": "1278747501642657792,1255542774432063488", // Edit Tweet IDs to look up
+        "tweet.fields": "lang,author_id", // Edit optional query parameters here
+        "user.fields": "created_at" // Edit optional query parameters here
     }
-
-    return tweets;
+    const res = await needle('get', endpointURL, params, {
+        headers: {
+            "User-Agent": "v2TweetLookupJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
 }
 
-module.exports = twitterRoute;
+(async() => {
+
+    try {
+        // Make request
+        const response = await getRequest();
+        console.dir(response, {
+            depth: null
+        });
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
